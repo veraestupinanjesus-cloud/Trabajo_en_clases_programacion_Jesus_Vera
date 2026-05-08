41eabadec95f188bf79037ac9bb9794cd74f3b1f
Jesus Vera <jesus.vera.estupinan@udla.edu.ec>
2026-05-08 14:35:17 -0500

    Finalizacion de la funcion suma

--- m.c
+++ m.c
@@ -2,5 +2,15 @@
 
 int sumar(int a, int b);
 int main (int argc, char *argv[]) {
+    int a, b;
+    printf("Ingrese el 1er numero:");
+    scanf("%d",&a);
+    printf("Ingrese el 2do numero:");
+    scanf("%d",&a);
+    printf("La suma es: %d", sumar(a,b));
     return 0;
 }
+int sumar(int a, int b){
+return a+b;
+}
+ 
